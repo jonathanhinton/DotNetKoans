1a430051ea124d6f14424fde1c5a3b89ddf7cf5d
Jonathan Hinton <hintonjonathan@gmail.com>
2016-02-15 15:13:22 -0800

    solution for 18

--- CSharp/AboutArrayAssignment.cs
+++ CSharp/AboutArrayAssignment.cs
@@ -36,7 +36,7 @@ namespace DotNetKoans.CSharp
             //Even though we don't specify types explicitly, the compiler
             //will pick one for us
             var names = new[] { "John", "Smith" };
-            Assert.Equal(typeof(FillMeIn), names.GetType());
+            Assert.Equal(typeof(String[]), names.GetType());
 
             //but only if it can. So this doesn't work
             //var array = new[] { "John", 1 };
