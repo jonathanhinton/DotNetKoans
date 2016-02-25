8752565d73d755b0517be79fc7515117e1a45574
Jonathan Hinton <hintonjonathan@gmail.com>
2016-02-24 20:29:07 -0600

    solution for 88

--- CSharp/AboutGenericContainers.cs
+++ CSharp/AboutGenericContainers.cs
@@ -24,7 +24,7 @@ namespace DotNetKoans.CSharp
 		{
 			ArrayList list = new ArrayList();
 			System.Reflection.MethodInfo method = list.GetType().GetMethod("Add");
-			Assert.Equal(typeof(FillMeIn), method.GetParameters()[0].ParameterType);
+			Assert.Equal(typeof(Object), method.GetParameters()[0].ParameterType);
 		}
 		[Koan(3)]
 		public void MustCastWhenRetrieving()
